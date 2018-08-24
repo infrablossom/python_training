e15c3a4b7ac307fe758dad0eeb275830b0f2c698
infrablossom <infrablossom@gmail.com>
2018-08-24 18:09:51 +0300

    Выделены классы для работы с группами и контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,17 +13,15 @@ def app(request):
 
 def test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
-    app.add_new_contact()
-    app.fill_contact_fields(Contact(firstname="Name", middlename="QA", lastname="Lastname", nick="Nick", title="Title", company="OOO", home="3434", mobile="3434", work="3434", fax="3434",
-                                 email="example@ex.com", phone2="home"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.add_new_contact()
+    app.contact.fill_fields(Contact(firstname="Name", middlename="QA", lastname="Lastname", nick="Nick", title="Title", company="OOO", home="3434", mobile="3434", work="3434", fax="3434",
+                                    email="example@ex.com", phone2="home"))
 
 
 def test2_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
-    app.add_new_contact()
-    app.fill_contact_fields(Contact(firstname="Tanya", middlename="", lastname="Ohlsen", nick="Blossom", title="", company="", home="999", mobile="888", work="", fax="",
-                                 email="example@cats.com", phone2="111"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.add_new_contact()
+    app.contact.fill_fields(Contact(firstname="Tanya", middlename="", lastname="Ohlsen", nick="Blossom", title="", company="", home="999", mobile="888", work="", fax="",
+                                    email="example@cats.com", phone2="111"))
