c56acda81b2fcb3e3a6c2dc2bb251eac497a9dc1
infrablossom <infrablossom@gmail.com>
2018-09-10 18:43:10 +0300

    Сделала все тесты создания, удаления и редактирования групп и контактов.

--- model/group.py
+++ model/group.py
@@ -1,8 +1,10 @@
 # вспомогательный класс для хранения свойств группы
 
+
 class Group:
 
     def __init__(self, name, header, footer):
         self.name = name
         self.header = header
         self.footer = footer
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
+
 from model.contact import Contact
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
@@ -17,6 +9,7 @@ def test_add_contact(app):
     app.contact.add_new_contact()
     app.contact.fill_fields(Contact(firstname="Name", middlename="QA", lastname="Lastname", nick="Nick", title="Title", company="OOO", home="3434", mobile="3434", work="3434", fax="3434",
                                     email="example@ex.com", phone2="home"))
+    app.session.logout()
 
 
 def test2_add_contact(app):
@@ -25,3 +18,4 @@ def test2_add_contact(app):
     app.contact.add_new_contact()
     app.contact.fill_fields(Contact(firstname="Tanya", middlename="", lastname="Ohlsen", nick="Blossom", title="", company="", home="999", mobile="888", work="", fax="",
                                     email="example@cats.com", phone2="111"))
+    app.session.logout()
