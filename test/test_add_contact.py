e60814b12c2df6feeeb4766ae3807521a468917e
infrablossom <infrablossom@gmail.com>
2018-09-14 18:45:09 +0300

    Интеллектуальная фикстура.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,17 +5,13 @@ from model.contact import Contact
 
 def test_add_contact(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.add_new_contact()
     app.contact.fill_fields(Contact(firstname="Name", middlename="QA", lastname="Lastname", nick="Nick", title="Title", company="OOO", home="3434", mobile="3434", work="3434", fax="3434",
                                     email="example@ex.com", phone2="home"))
-    app.session.logout()
 
 
 def test2_add_contact(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
     app.contact.add_new_contact()
     app.contact.fill_fields(Contact(firstname="Tanya", middlename="", lastname="Ohlsen", nick="Blossom", title="", company="", home="999", mobile="888", work="", fax="",
                                     email="example@cats.com", phone2="111"))
-    app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,7 +2,5 @@ from model.group import Group
 
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="111", header="111", footer="111"))
-    app.session.logout()
 
