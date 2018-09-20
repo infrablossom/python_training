0becc89aa57b84a9cbaee783900048f86003b85d
infrablossom <infrablossom@gmail.com>
2018-09-20 13:16:54 +0300

    Спроблема с преобразованием типов

--- model/group.py
+++ model/group.py
@@ -1,5 +1,5 @@
 # вспомогательный класс для хранения свойств группы
-
+from sys import maxsize
 
 class Group:
 
@@ -14,5 +14,12 @@ class Group:
 
     # определила функцию сравнения самих объектов, а не их физического расположения в памяти
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
 
