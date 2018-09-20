565840ce8231868f7b963006b98051cd8502e64f
infrablossom <infrablossom@gmail.com>
2018-09-20 11:53:28 +0300

    Загрузка информации из тестируемого приложения и проверка равенства объектов и списков

--- model/group.py
+++ model/group.py
@@ -3,8 +3,16 @@
 
 class Group:
 
-    def __init__(self, name=None, header=None, footer=None):
+    def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
         self.header = header
         self.footer = footer
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    # определила функцию сравнения самих объектов, а не их физического расположения в памяти
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
 
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,7 +2,14 @@ from model.group import Group
 
 
 def test_delete_first_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count_groups() == 0:
         app.group.create(Group(name="test"))
     app.group.delete_first_group()
+    new_groups = app.group.get_group_list()
+    # не удаляем строку проверки длины 2 списков для перестраховки
+    # и более понятного эксепшена в случае отсутствия элементов
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []
+    assert old_groups == new_groups
 
